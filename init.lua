31d17de9fdfb4206187d575ad74dccbc2863ba4e
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-17 15:42:01 +0530

    perf: requiring with Error Checks

--- init.lua
+++ init.lua
@@ -1,31 +1,38 @@
--- CORE
-require "core.plugins"
-require "core.options"
-require "core.colorscheme"
-require "core.keymaps"
-require "core.cmds"
-require "core.files"
+for _, source in ipairs({
+	-- CORE
+	"core.plugins",
+	"core.options",
+	"core.colorscheme",
+	"core.keymaps",
+	"core.cmds",
+	"core.files",
 
--- PLUGS
-require "core.alpha"
-require "core.toggler"
-require "core.todo"
-require "core.cmp"
-require "core.telescope"
-require "core.treesitter"
-require "core.nvim-tree"
-require "core.null-ls"
-require "core.gitsigns"
-require "core.toggleterm"
-require "core.lspInstaller"
-require "core.lsp"
-require "core.lsp-saga"
+	-- PLUGS
+	"core.alpha",
+	"core.toggler",
+	"core.todo",
+	"core.cmp",
+	"core.telescope",
+	"core.treesitter",
+	"core.nvim-tree",
+	"core.null-ls",
+	"core.gitsigns",
+	"core.toggleterm",
+	"core.lspInstaller",
+	"core.lsp",
+	"core.lsp-saga",
+}) do
+	local status_ok, fault = pcall(require, source)
+	if not status_ok then
+		vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+	end
+end
 
 -- EXTRAS / OTHERS
 vim.notify = require("notify")
-vim.cmd('source $HOME/.abbreviations.vim')
+vim.cmd("source $HOME/.abbreviations.vim")
 
 -- NOTE: Needs to be checked
 require("notify").setup({
-    background_colour = "#000000",
+	background_colour = "#000000",
 })
