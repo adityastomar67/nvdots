4bb8c3607c65668e7bd39442e74107902aa1c674
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-27 08:35:34 +0530

    Updates file structure

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,53 +3,49 @@ local opt = vim.opt
 local wo = vim.wo
 
 ---- :help options
-opt.backup = false                          -- creates a backup file
-opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-opt.fileencoding = "utf-8"                  -- the encoding written to a file
-opt.ignorecase = true                       -- ignore case in search patterns
-opt.mouse = "a"                             -- allow the mouse to be used in neovim
-opt.pumheight = 10                          -- pop up menu height
-opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
-opt.smartcase = true                        -- smart case
-opt.smartindent = true                      -- make indenting smarter again
-opt.splitbelow = false                       -- force all horizontal splits to go below current window
-opt.splitright = true                       -- force all vertical splits to go to the right of current window
-opt.swapfile = false                        -- creates a swapfile
-opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-opt.undofile = true                         -- enable persistent undo
-opt.updatetime = 300                        -- faster completion (4000ms default)
-opt.expandtab = true                        -- convert tabs to spaces
-opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
-opt.tabstop = 4                             -- insert 2 spaces for a tab
-opt.number = false                          -- set numbered lines
-opt.relativenumber = false                  -- set relative numbered lines
-opt.numberwidth = 1                         -- set number column width to 2 {default 4}
-opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-opt.wrap = true                             -- display lines as one long line
-opt.scrolloff = 8                           -- is one of my fav
-opt.writebackup = false                     -- when file is edited by some program (or was written to file while editing with some program), it's not allowed to be edited
-opt.sidescrolloff = 8                       -- how many lines to scroll when you scroll past the end of the screen
-opt.shortmess:append "c"                    -- show the current cursor position
-opt.background = "dark"                     -- set the background color
-opt.fillchars = { eob = " " }               -- set the fill character for the end of the line
-opt.laststatus = 0                          -- set the last status line to 0
-opt.hidden = true                           -- hide the status line
-opt.whichwrap:append "<>[]hl"               -- set the wrap characters
-opt.signcolumn = "yes"                      -- show the sign column
-opt.foldmethod = "indent"
-opt.foldnestmax = 10
-opt.foldenable = false
-opt.winblend = 0
--- opt.wildoptions = 'pum'
--- opt.pumblend = 5
-opt.foldlevel = 2
-opt.cole  = 1                               -- Conceal applied
-opt.cursorline = true                       -- highlight the current line
-opt.cursorlineopt = "number"                -- show the line numbers highlighted
-opt.formatoptions = "l"
-opt.formatoptions = opt.formatoptions
+opt.backup         = false                     -- creates a backup file
+opt.clipboard      = "unnamedplus"             -- allows neovim to access the system clipboard
+opt.completeopt    = { "menuone", "noselect" } -- mostly just for cmp
+opt.fileencoding   = "utf-8"                   -- the encoding written to a file
+opt.ignorecase     = true                      -- ignore case in search patterns
+opt.mouse          = "a"                       -- allow the mouse to be used in neovim
+opt.pumheight      = 10                        -- pop up menu height
+opt.showmode       = false                     -- we don't need to see things like -- INSERT -- anymore
+opt.smartcase      = true                      -- smart case
+opt.smartindent    = true                      -- make indenting smarter again
+opt.splitbelow     = false                     -- force all horizontal splits to go below current window
+opt.splitright     = true                      -- force all vertical splits to go to the right of current window
+opt.swapfile       = false                     -- creates a swapfile
+opt.termguicolors  = true                      -- set term gui colors (most terminals support this)
+opt.timeoutlen     = 1000                      -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.undofile       = true                      -- enable persistent undo
+opt.updatetime     = 300                       -- faster completion (4000ms default)
+opt.expandtab      = true                      -- convert tabs to spaces
+opt.shiftwidth     = 4                         -- the number of spaces inserted for each indentation
+opt.tabstop        = 4                         -- insert 2 spaces for a tab
+opt.number         = false                     -- set numbered lines
+opt.relativenumber = false                     -- set relative numbered lines
+opt.numberwidth    = 1                         -- set number column width to 2 {default 4}
+opt.signcolumn     = "yes"                     -- always show the sign column, otherwise it would shift the text each time
+opt.wrap           = true                      -- display lines as one long line
+opt.scrolloff      = 8                         -- is one of my fav
+opt.writebackup    = false                     -- when file is edited by some program (or was written to file while editing with some program), it's not allowed to be edited
+opt.sidescrolloff  = 8                         -- how many lines to scroll when you scroll past the end of the screen
+opt.background     = "dark"                    -- set the background color
+opt.fillchars      = { eob = " " }             -- set the fill character for the end of the line
+opt.laststatus     = 0                         -- set the last status line to 0
+opt.hidden         = true                      -- hide the status line
+opt.signcolumn     = "yes"                     -- show the sign column
+opt.foldmethod     = "indent"
+opt.foldnestmax    = 10
+opt.foldenable     = false
+opt.winblend       = 0
+opt.foldlevel      = 2
+opt.cole           = 1                         -- Conceal applied
+opt.cursorline     = true                      -- highlight the current line
+opt.cursorlineopt  = "number"                  -- show the line numbers highlighted
+opt.formatoptions  = "l"
+opt.formatoptions  = opt.formatoptions
     - "a" -- Auto formatting is BAD.
     - "t" -- Don't auto format my code. I got linters for that.
     + "c" -- In general, I like it when comments respect textwidth
@@ -59,6 +55,8 @@ opt.formatoptions = opt.formatoptions
     + "n" -- Indent past the formatlistpat, not underneath it.
     + "j" -- Auto-remove comments if possible.
     - "2" -- I'm not in gradeschool anymore
+opt.shortmess:append "c"                    -- show the current cursor position
+opt.whichwrap:append "<>[]hl"               -- set the wrap characters
 opt.guicursor = {
     "n-v:block",
     "i-c-ci-ve:ver25",
@@ -69,19 +67,19 @@ opt.guicursor = {
 }
 
 -- IMPROVE NEOVIM STARTUP
-vim.g.loaded_python_provier = 0
-vim.g.loaded_python3_provider = 0
-vim.g.python_host_skip_check = 1
-vim.g.python_host_prog='/bin/python2'
-vim.g.python3_host_skip_check = 1
-vim.g.python3_host_prog='/bin/python3'
-vim.opt.pyxversion=3
-vim.g.EditorConfig_core_mode = 'external_command'
-vim.g.matchparen_timeout = 20
+vim.g.loaded_python_provier     = 0
+vim.g.loaded_python3_provider   = 0
+vim.g.python_host_skip_check    = 1
+vim.g.python_host_prog          = '/bin/python2'
+vim.g.python3_host_skip_check   = 1
+vim.g.python3_host_prog         = '/bin/python3'
+vim.g.EditorConfig_core_mode    = 'external_command'
+vim.g.matchparen_timeout        = 20
 vim.g.matchparen_insert_timeout = 20
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-vim.g.nojoinspaces = true
+vim.g.do_filetype_lua           = 1
+vim.g.did_load_filetypes        = 0
+vim.g.nojoinspaces              = true
+vim.opt.pyxversion                = 3
 
 -- Give me some fenced codeblock goodness
 vim.g.markdown_fenced_languages = {
